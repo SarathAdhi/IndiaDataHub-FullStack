559247b79cfdaf86bf2a59f7c9913ff525ae3c47
SarathAdhi <s.sarathadhithya@gmail.com>
2024-08-29 13:18:42 +0530

    frontend: minor changes

--- client/src/pages/ForgotPasswordPage.tsx
+++ client/src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import PageLayout from "@/layouts/PageLayout";
+import { LockKeyholeIcon } from "lucide-react";
+import toast from "react-hot-toast";
+
 const ForgotPasswordPage = () => {
-  return <div>ForgotPasswordPage</div>;
+  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // toast.success("Password reset link sent to your email");
+    toast.error("This feature is not implemented yet");
+  }
+
+  return (
+    <PageLayout className="items-center">
+      <form
+        onSubmit={handleFormSubmit}
+        className="mt-20 w-full max-w-sm space-y-10"
+      >
+        <div className="flex flex-col items-center text-center">
+          <div className="grid place-content-center bg-primary text-primary-foreground p-1 size-10 rounded-full">
+            <LockKeyholeIcon />
+          </div>
+
+          <h5>Forgot Password</h5>
+        </div>
+
+        <div className="space-y-4">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            required
+          />
+
+          <Button className="w-full" size="sm">
+            Reset Password
+          </Button>
+        </div>
+      </form>
+    </PageLayout>
+  );
 };
 
 export default ForgotPasswordPage;
